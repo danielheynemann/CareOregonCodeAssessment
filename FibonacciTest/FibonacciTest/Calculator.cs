121ab2a724a6bd0487e0f6424498fc6f982bcfff
danielheynemann <53133689+danielheynemann@users.noreply.github.com>
2019-07-20 21:45:41 -0700

    Add files via upload

---
+++ FibonacciTest/FibonacciTest/Calculator.cs
@@ -0,0 +1,21 @@
+﻿namespace FibonacciTest
+{
+    public class Calculator
+    {
+        public int CalculateFibonacci(int position)
+        {
+            int zeroBasedPosition = position - 1;
+            int[] sequence = new int[position];
+
+            sequence[0] = 1;
+            sequence[1] = 1;
+
+            for (int counter = 2; counter <= zeroBasedPosition; counter++)
+            {
+                sequence[counter] = sequence[counter - 2] + sequence[counter - 1];
+            }
+
+            return sequence[zeroBasedPosition];
+        }
+    }
+}
---
+++ FibonacciTest/FibonacciTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FibonacciTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] requestedPositions = { 10, 40 };
+            
+            Calculator codeAssessmentCalculator = new Calculator();
+            
+            foreach (int position in requestedPositions)
+            {
+                Console.WriteLine(codeAssessmentCalculator.CalculateFibonacci(position));
+            }
+
+            Console.ReadLine();
+        }         
+    }
+}
